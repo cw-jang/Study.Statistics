8d8eb5a89d5886d3882ab17a5b9d6568b957949b
jangchan <changwan.jang@gmail.com>
2015-03-11 23:53:53 +0900

    7.5 적합도 검정

--- R을이용한데이터처리&분석실무(서민구)/ch07/7_05.r
+++ R을이용한데이터처리&분석실무(서민구)/ch07/7_05.r
@@ -22,3 +22,74 @@ chisq.test(table(survey$W.Hnd),p=c(.3,.7))
 # X-squared = 56.2518, df = 1, p-value = 6.376e-14
 
 # p-value < 0.05이므로 글씨를 쓰는 사람과 오른손으로 쓰는 사람의 비가 30% : 70%라는 귀무가설을 기각한다.
+
+
+### 샤피로 윌크 검정
+# 샤피로 윌크 검정Shapiro-Wilk Test은 표본이 정규 분포로부터 추출된 것인지 테스트하기 위한 방법이다.
+# 검정은 shapiro.test() 함수를 사용하며 이때 귀무가설은 주어진 데이터가 정규 분포로부터의 표본이라는 것이다.
+
+# shapiro.test: 데이터가 정규 분포를 따르는지 샤피로 윌크 검정을 수행한다. 귀무가설은 정규 분포를 따른다는 것이다.
+#  shapiro.test(
+#	x # 숫자벡터
+# )
+
+# 다음은 정규분포를 따르는 1000개의 난수를 발생시킨 뒤 이 숫자들이 정규 분포를 따르는지 
+# 샤피로 윌크 검정을 수행한 예다.
+shapiro.test(rnorm(1000))
+# 
+#         Shapiro-Wilk normality test
+# 
+# data:  rnorm(1000)
+# W = 0.9976, p-value = 0.1646
+
+# p-value > 0.05이므로 데이터가 정규 분포를 따른다는 귀무가설을 기각할 수 없다.
+# shapiro.test() 외에도 nortest 패키지에는 앤더스 달링 검정Anderson-Darling Test, 
+# 피어슨 카이 제곱 검정 Pearson Chi-Square Test 등을 사용해 정규성을 검정하는 다양한 함수가 있다.
+
+
+### 콜모고로프 스미르노프 검정
+# 콜모고로프 스미르노프 검정Kolmogorov-Smirnov Test(K-S Test라고도 부름)은 데이터의 누적 분포 함수
+# (7.1 난수생성 및 분포 함수)와 비교하고자 하는 분포의 누적 분포 함수Cumulative Distribution Function
+# 간의 최대 거리를 통계량으로 사용하는 가설 검정 방법이다.
+
+# ks.test : K-S검정을 수행. 귀무가설은 x,y에 차이가 없다고 보는 것이다.
+#	x, # 숫자 벡터
+#	y, # 숫자 벡터 또는 누적 분포 함수(예를 들면, pnorm)
+#	..., # y에 지정한 누적 분포 함수에 넘겨줄 파라미터
+#	alternative=c("two.sided", "less", "greater) # 대립가설
+
+# 다음은 정규 분포를 따르는 두 난수 데이터 간에 분포가 동일한지를 살펴본 예다.
+ks.test(rnorm(100), rnorm(100))
+
+#         Two-sample Kolmogorov-Smirnov test
+# 
+# data:  rnorm(100) and rnorm(100)
+# D = 0.11, p-value = 0.5806
+# alternative hypothesis: two-sided
+
+# p-value > 0.05이므로 두 난수가 같은 분포라는 귀무가설을 기각할 수 없다. 
+# 다음은 정규 분포와 균등 분포Uniform Distribution간의 비교를 해보자.
+ks.test(rnorm(100), runif(100))
+
+#         Two-sample Kolmogorov-Smirnov test
+# 
+# data:  rnorm(100) and runif(100)
+# D = 0.5, p-value = 2.778e-11
+# alternative hypothesis: two-sided
+
+# p-value가 0.05보다 작아 서로 다른 분포로 판단되었다.
+
+# 다음은 K-S 검정을 사용해 데이터가 평균 0, 분산 1인 정규 분포로부터 뽑은 표본인지 확인하는 예다.
+ks.test(rnorm(1000), "pnorm", 0, 1)
+
+#         One-sample Kolmogorov-Smirnov test
+# 
+# data:  rnorm(1000)
+# D = 0.0336, p-value = 0.2098
+# alternative hypothesis: two-sided
+
+# 귀무가설을 기각할 수 없어, 주어진 rnorm(100)은 평균0, 분산1인 정규 분포로부터의 표본이라고 결론.
+
+
+### Q-Q도 
+# Q-Q도 
